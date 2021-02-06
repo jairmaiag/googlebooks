9cb94acd244842f5078b311802266cc1f503dcc6
Jair <jairmaiag@gmail.com>
2021-02-06 15:40:15 -0300

    Ajuste no alerta

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
+	function alertar(textoAviso){
+		let caixa = '<div class="alert alert-danger" role="alert" id="aviso">';
+		caixa+='<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+		caixa+=`<p>${textoAviso}</p>`;
+		caixa+='</div>';
+		$('#alertas').append(caixa);
+	};
+		
 	
 	$('#btnLocalizar').click(function(){
 		event.preventDefault();
 		let texto = $('#txtLocalizar').val();
 		
+		if(!texto){
+			alertar('É preciso informar um título de livro ou nome de autor');
+			return;
+		}
+		$("#aviso").remove();
 		for(let i=0;i<=9;i++){
 			let objeto = `<div class="col-sm-6 col-md-4">`;
 			objeto+='<div class="thumbnail">';
@@ -14,7 +27,5 @@ $(document).ready(function() {
 			objeto+='</div></div></div>';
 			$('#divLinha').append(objeto);			
 		}
-		console.log('Inclusa');
-		
 	});
 });
