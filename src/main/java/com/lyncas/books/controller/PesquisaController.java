ab31e2f4fd3b22e69c1f01e6daa8745fbd4bedec
Jair <jairmaiag@gmail.com>
2021-02-06 18:08:40 -0300

    Requisição funcionando

--- src/main/java/com/lyncas/books/controller/PesquisaController.java
+++ src/main/java/com/lyncas/books/controller/PesquisaController.java
@@ -7,18 +7,24 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TesteController {
-	final String GOOGLE_API = "https://www.googleapis.com/books/v1/"; 
-	@RequestMapping(value = "/teste", method = RequestMethod.GET)
-	String teste() throws IOException, InterruptedException {
+public class PesquisaController {
+	final String GOOGLE_API = "https://www.googleapis.com/books";
+	final String GOOGLE_API_VOLUMES = GOOGLE_API+"/v1/volumes/?q=";
+	
+	@RequestMapping(value = "/api/pesquisar/{texto}", method = RequestMethod.GET)
+	String pesquisar(@PathVariable("texto") String texto) throws IOException, InterruptedException {
+		if(texto == null) {
+			return "{mensage:\"Texto a pesquisar precisa ser enviado\"}";
+		}
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create(GOOGLE_API.concat("volumes?q=quilting")))
+				.uri(URI.create(GOOGLE_API_VOLUMES.concat(texto)))
 				.header("Content-Type", "application/json").build();
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 		return response.body();
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,31 +1,72 @@
 $(document).ready(function() {
-	function alertar(textoAviso){
+	function alertar(textoAviso) {
 		let caixa = '<div class="alert alert-danger" role="alert" id="aviso">';
-		caixa+='<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-		caixa+=`<p>${textoAviso}</p>`;
-		caixa+='</div>';
+		caixa += '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+		caixa += `<p>${textoAviso}</p>`;
+		caixa += '</div>';
 		$('#alertas').append(caixa);
 	};
-		
 	
-	$('#btnLocalizar').click(function(){
+	function montarLivro(livro){
+		let volumeInfo = livro.volumeInfo;
+		let image = volumeInfo.imageLinks.thumbnail;
+		let title = volumeInfo.title;
+		let description = volumeInfo.description;
+		if(description){
+			if(description.length > 160){
+				description = description.substring(0,150);
+			}
+		}else{
+			description='';
+		}
+		
+		let objeto = `<div class="col-sm-6 col-md-4">`;
+		objeto += '<div class="thumbnail">';
+		objeto += `<img src="${image}" alt="${title}">`;
+		objeto += '<div class="caption">';
+		objeto += `<h3>${title}</h3>`;
+		objeto += `<p>${description}</p>`;
+		objeto += '<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>';
+		objeto += '</div></div></div>';
+		return objeto;
+	};
+	
+	$('#btnLocalizar').click(function() {
 		event.preventDefault();
 		let texto = $('#txtLocalizar').val();
-		
-		if(!texto){
+		if (!texto) {
 			alertar('É preciso informar um título de livro ou nome de autor');
 			return;
 		}
 		$("#aviso").remove();
-		for(let i=0;i<=9;i++){
+		
+		$.ajax({
+			type : "GET",
+			url : `/api/pesquisar/${texto}`,
+			data : '$format=json',
+			dataType : 'json',
+			success : function(data) {
+					$.each(data.items, function(i, livro) {
+						//console.log(livro);
+						console.log(livro);
+						$('#divLinha').append(montarLivro(livro));
+					});
+					/*
+					$("#results").text(total+" found");
+					*/
+			}
+			});
+		/*
+		for (let i = 0; i <= 9; i++) {
 			let objeto = `<div class="col-sm-6 col-md-4">`;
-			objeto+='<div class="thumbnail">';
-			objeto+='<div class="caption">';
-			objeto+='<h3>Titulo do livro</h3>';
-			objeto+='<p>Descrição</p>';
-			objeto+='<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>';
-			objeto+='</div></div></div>';
-			$('#divLinha').append(objeto);			
+			objeto += '<div class="thumbnail">';
+			objeto += '<div class="caption">';
+			objeto += '<h3>Titulo do livro</h3>';
+			objeto += '<p>Descrição</p>';
+			objeto += '<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>';
+			objeto += '</div></div></div>';
+			$('#divLinha').append(objeto);
 		}
+		*/
 	});
 });
