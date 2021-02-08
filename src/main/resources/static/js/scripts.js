632836b0c0176be8c069db6a44d3cbbffd95e2b6
Jair <jairmaiag@gmail.com>
2021-02-08 10:50:51 -0300

    Ajustes para utilização do frontEnd

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,53 +1,50 @@
-$(document).ready(function() {
-	function alertar(textoAviso) {
-		let caixa = '<div class="alert alert-danger" role="alert" id="aviso">';
-		caixa += '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-		caixa += `<p>${textoAviso}</p>`;
-		caixa += '</div>';
-		$('#alertas').append(caixa);
-	};
-
-	function montarLivro(livro) {
-		console.log(livro);
-		let volumeInfo = livro.volumeInfo;
-		let preview = volumeInfo.previewLink;
-		let nomesAuthors = '';
-		let authors = volumeInfo.authors;
-		$.each(authors, function(i, nome) {
-			nomesAuthors += nome + ', ';
-		});
-		nomesAuthors = nomesAuthors.substring(0, nomesAuthors.length - 2)
-		let image='';
-		if(volumeInfo.imageLinks && volumeInfo.imageLinks.thumbnail){
-			image = volumeInfo.imageLinks.thumbnail;
-		}
-		let title = volumeInfo.title;
-		if (title.length > 80) {
-			title = title.substring(0, 80) + ' ...';
-		}
-		let description = volumeInfo.description;
-		if (description) {
-			if (description.length > 160) {
-				description = description.substring(0, 150) + ' ...';
-			}
-		} else {
-			description = 'Livro sem descrição';
-		}
-		let objeto = `<div class="col-sm-6 col-md-4">`;
-		objeto += '<div class="thumbnail">';
-		objeto += `<a href="${preview}" target="_blank"><img src="${image}" alt="${title}" width="128" height="182"></a>`;
-		objeto += '<div class="caption">';
-		objeto += `<h3>${title}</h3>`;
-		objeto += `<p><strong>${nomesAuthors}</strong></p>`;
-		objeto += `<p>${description}</p>`;
-		objeto += '<p><a href="#" class="btn btn-primary" role="button">Favorito</a> ';
-		objeto += `<a href="${preview}" class="btn btn-default" role="button" target="_blank">Prévia</a></p>`;
-		objeto += '</div></div></div>';
-		return objeto;
-	};
-	function listarFavoritos(){
-		console.log('Chamado a listagem');
+function montarLivro(livro) {
+	console.log(livro);
+	let volumeInfo = livro.volumeInfo;
+	let preview = volumeInfo.previewLink;
+	let nomesAuthors = '';
+	let authors = volumeInfo.authors;
+	$.each(authors, function(i, nome) {
+		nomesAuthors += nome + ', ';
+	});
+	nomesAuthors = nomesAuthors.substring(0, nomesAuthors.length - 2)
+	let image='';
+	if(volumeInfo.imageLinks && volumeInfo.imageLinks.thumbnail){
+		image = volumeInfo.imageLinks.thumbnail;
 	}
+	let title = volumeInfo.title;
+	if (title.length > 80) {
+		title = title.substring(0, 80) + ' ...';
+	}
+	let description = volumeInfo.description;
+	if (description) {
+		if (description.length > 160) {
+			description = description.substring(0, 150) + ' ...';
+		}
+	} else {
+		description = 'Livro sem descrição';
+	}
+	let objeto = `<div class="col-sm-6 col-md-4">`;
+	objeto += '<div class="thumbnail">';
+	objeto += `<a href="${preview}" target="_blank"><img src="${image}" alt="${title}" width="128" height="182"></a>`;
+	objeto += '<div class="caption">';
+	objeto += `<h3>${title}</h3>`;
+	objeto += `<p><strong>${nomesAuthors}</strong></p>`;
+	objeto += `<p>${description}</p>`;
+	objeto += '<p><a href="#" class="btn btn-primary" role="button">Favorito</a> ';
+	objeto += `<a href="${preview}" class="btn btn-default" role="button" target="_blank">Prévia</a></p>`;
+	objeto += '</div></div></div>';
+	return objeto;
+};
+function alertar(textoAviso) {
+	let caixa = '<div class="alert alert-danger" role="alert" id="aviso">';
+	caixa += '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+	caixa += `<p>${textoAviso}</p>`;
+	caixa += '</div>';
+	$('#alertas').append(caixa);
+};
+
+$(document).ready(function() {
 	$('#btnLimpar').click(function() {
 
 	});
