3e82829acea47f6c8c96083c50736295f71d1cb3
Jair <jairmaiag@gmail.com>
2021-02-09 09:55:25 -0300

    Caminho para pesquisa por id do volume

--- src/main/java/com/lyncas/books/util/Constantes.java
+++ src/main/java/com/lyncas/books/util/Constantes.java
@@ -3,5 +3,5 @@ package com.lyncas.books.util;
 public interface Constantes {
 	public static final String GOOGLE_API = "https://www.googleapis.com/books";
 	public static final String GOOGLE_API_VOLUMES = GOOGLE_API+"/v1/volumes/?q=";
-
+	public static final String GOOGLE_API_ID = GOOGLE_API+"/v1/volumes/";
 }
