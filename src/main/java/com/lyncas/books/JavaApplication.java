b00945d773994e2224a11054c1d09b269782cfbb
Jair <jairmaiag@gmail.com>
2021-02-09 09:55:46 -0300

    Limpeza de código

--- src/main/java/com/lyncas/books/JavaApplication.java
+++ src/main/java/com/lyncas/books/JavaApplication.java
@@ -1,6 +1,5 @@
 package com.lyncas.books;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,15 +7,10 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.lyncas.books.repository.BookRepository;
-
 @ComponentScan({"com.lyncas.books"})
 @SpringBootApplication
 @EnableJpaRepositories
 public class JavaApplication extends SpringBootServletInitializer {
-
-	@Autowired
-	private BookRepository bookRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaApplication.class, args);
